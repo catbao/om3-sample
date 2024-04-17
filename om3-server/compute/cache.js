b90da44756d834b0155f35131b1b1eb3e79b3272
catbao <1925826455@qq.com>
2024-04-17 14:20:41 +0800

    fixing cache.js

--- om3-server/compute/cache.js
+++ om3-server/compute/cache.js
@@ -20,19 +20,19 @@ class CustomCache {
       if(!this.indexPointsMap.has(value.level)){
         this.indexPointsMap.set(value.level, value);
       }
-      if (this.cacheMap.has(key)) {
-        // 如果缓存中已存在该键，更新值并调整顺序
-        const node = this.cacheMap.get(key);
-        node.freq += 1;
-        // this.updateNodeOrder(node);
-      } else {
-        this.cacheMap.set(key, value);
-        this.addToSortedPosition(value);
-        // 如果缓存满了，先移出最不符合条件的数据
-        if (this.cacheMap.size >= this.capacity) {
-          this.evict();
-        }
+      // if (this.cacheMap.has(key)) {
+      //   // 如果缓存中已存在该键，更新值并调整顺序
+      //   const node = this.cacheMap.get(key);
+      //   node.freq += 1;
+      //   // this.updateNodeOrder(node);
+      // } else {
+      this.cacheMap.set(key, value);
+      this.addToSortedPosition(value);
+      // 如果缓存满了，先移出最不符合条件的数据
+      if (this.cacheMap.size >= this.capacity) {
+        this.evict();
       }
+      // }
     }
   
     getMaxKey() {
@@ -74,38 +74,63 @@ class CustomCache {
     changeFreq(node){
       let prev = this.indexPointsMap.get(node.level);
       if(prev == node){
+        // console.log(0)
         let later = node.next;
-        if(node.next!== undefined && later.freq < node.freq||(later.freq === node.freq && later.index < node.index)){
+        if(later === undefined) return;
+        // if(later.freq < node.freq||(later.freq === node.freq && later.index < node.index)){
+        if(later.freq < node.freq){
           // this.head = later;
           this.indexPointsMap.set(node.level, later);
           node.next = later.next;
+          later.next.last = node;
           later.next = node;
+          node.last = later;
         }
         prev = later;
         later = node.next;
-        if(later === undefined) return;
-        while(node.next!==undefined && later.freq < node.freq||(later.freq === node.freq && later.index < node.index)){
-          prev.next = later;
-          node.next = later.next;
-          later.next = node;
-          prev = prev.next;
-          node = node.next;
-          later = later.next;
+        
+        while(node.next!==undefined){
+          if(later.freq < node.freq){
+            prev.next = later;
+            later.last = prev;
+            node.next = later.next;
+            later.next.last = node;
+            later.next = node;
+            node.last = later
+            prev = prev.next;
+            // node = node.next;
+            later = node.next;
+          }
+          else{
+            break;
+          }
         }
         return;
       }
 
-      while(prev.next != node){
-        prev = prev.next;
-      }
+      // while(prev.next != node){
+      //   prev = prev.next;
+      // }
+      prev = node.last;
       let later = node.next;
-      while(node.next!==undefined && later.freq < node.freq||(later.freq === node.freq && later.index < node.index)){
-        prev.next = later;
-        node.next = later.next;
-        later.next = node;
-        prev = prev.next;
-        node = node.next;
-        later = later.next;
+      if(later === undefined) return;
+      while(node.next!==undefined){
+        // console.log(1);
+        if(later.freq < node.freq){
+          console.log(2);
+          prev.next = later;
+          later.last = prev;
+          node.next = later.next;
+          later.next.last = node;
+          later.next = node;
+          node.last = later
+          prev = prev.next;
+          // node = node.next;
+          later = node.next;
+        }
+        else{
+          break;
+        }
       }
     }
   
@@ -119,11 +144,14 @@ class CustomCache {
   
       while (
         current &&
-        (current.level > node.level ||
-          (current.level === node.level && current.freq < node.freq) ||(current.freq === node.freq && current.index < node.index))
+        // (current.level > node.level ||
+          // (current.level === node.level && current.freq < node.freq) ||(current.freq === node.freq && current.index < node.index))
+          (current.freq < node.freq)
       ) {
+        // console.log(0);
         prev = current;
         current = current.next;
+        current.last = prev;
       }
   
       if (!current) {
@@ -131,15 +159,19 @@ class CustomCache {
         // this.addToEnd(level, node);
         current = node;
         prev.next = current;
+        current.last = prev;
       } else {
         // if(current === this.head){
         if(current === this.indexPointsMap.get(level)){
-          this.head = node;
+          this.indexPointsMap.set(level,node);
           node.next = current;
+          current.last = node;
         }
         else{
           prev.next = node;
           node.next = current;
+          current.last = node;
+          node.last = prev;
         }
       }
     }
@@ -161,7 +193,7 @@ class CustomCache {
     }
 }
 
-const testCache = new CustomCache(10);
+const testCache = new CustomCache(100000);
 // testCache.insert("key1", "value1");
 
 module.exports = { testCache }
